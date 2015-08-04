ef13016d9ae8b152fd57607ba320f90fb2295545
Vagif Abilov <vagif.abilov@gmail.com>
2015-08-04 10:33:01 +0200

    Improved expression parsing to support open type scenarios.

--- Simple.OData.Client.Core/Expressions/ExpressionContext.cs
+++ Simple.OData.Client.Core/Expressions/ExpressionContext.cs
@@ -4,11 +4,13 @@
     {
         public ISession Session { get; set; }
         public EntityCollection EntityCollection { get; set; }
+        public string DynamicPropertiesContainerName { get; set; }
 
-        public ExpressionContext(ISession session, EntityCollection entityCollection = null)
+        public ExpressionContext(ISession session, EntityCollection entityCollection = null, string dynamicPropertiesContainerName = null)
         {
             this.Session = session;
             this.EntityCollection = entityCollection;
+            this.DynamicPropertiesContainerName = dynamicPropertiesContainerName;
         }
     }
 }
