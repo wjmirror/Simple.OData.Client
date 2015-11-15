9798188f632d30c577e7ce29d854dc80b6a421d4
Vagif Abilov <vagif.abilov@gmail.com>
2015-11-15 19:45:18 +0100

    Incremented version number.

--- CommonAssemblyVersion.cs
+++ CommonAssemblyVersion.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.12.0.0")]
-[assembly: AssemblyFileVersion("4.12.0.0")]
-[assembly: AssemblyInformationalVersion("4.12.0.0")]
+[assembly: AssemblyVersion("4.13.0.0")]
+[assembly: AssemblyFileVersion("4.13.0.0")]
+[assembly: AssemblyInformationalVersion("4.13.0.0")]
