73a2e53adab6bcd2633c0100c9225e686edbf498
Vagif Abilov <vagif.abilov@gmail.com>
2016-02-03 14:53:30 +0100

    Updated version number.

--- CommonAssemblyVersion.cs
+++ CommonAssemblyVersion.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.14.1.0")]
-[assembly: AssemblyFileVersion("4.14.1.0")]
-[assembly: AssemblyInformationalVersion("4.14.1.0")]
+[assembly: AssemblyVersion("4.14.2.0")]
+[assembly: AssemblyFileVersion("4.14.2.0")]
+[assembly: AssemblyInformationalVersion("4.14.2.0")]
