026940f91d4e2c31ebdf79ff10e3e0dbdb9c9bfb
Vagif Abilov <vagif.abilov@gmail.com>
2016-09-01 13:55:51 +0200

    Version bumb.

--- CommonAssemblyVersion.cs
+++ CommonAssemblyVersion.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.23.4.0")]
-[assembly: AssemblyFileVersion("4.23.4.0")]
-[assembly: AssemblyInformationalVersion("4.23.4.0")]
+[assembly: AssemblyVersion("4.24.0.0")]
+[assembly: AssemblyFileVersion("4.24.0.0")]
+[assembly: AssemblyInformationalVersion("4.24.0.0")]
