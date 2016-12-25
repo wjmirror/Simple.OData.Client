f8e37f742768e866a4486dfa7310706cf98532e5
Vagif Abilov <vagif.abilov@gmail.com>
2016-12-25 22:56:49 +0100

    Bumped version number.

--- CommonAssemblyVersion.cs
+++ CommonAssemblyVersion.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.24.0.0")]
-[assembly: AssemblyFileVersion("4.24.0.0")]
-[assembly: AssemblyInformationalVersion("4.24.0.0")]
+[assembly: AssemblyVersion("4.24.1.0")]
+[assembly: AssemblyFileVersion("4.24.1.0")]
+[assembly: AssemblyInformationalVersion("4.24.1.0")]
